5ef32f72acf08f9a8848592e5f29bd6f074be775
ajsbsd@gmail.com <ajsbsd@gmail.com>
2025-05-11 20:19:34 +0000

    0

---
+++ 0.sh
@@ -0,0 +1,8 @@
+python -m venv .
+source /home/ai/bin/activate
+git clone https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct
+git branch -M main
+git push -u origin main
+git branch -M main
+git config --global user.email "ajsbsd@gmail.com"
+git config --global user.name "ajsbsd@gmail.com"
